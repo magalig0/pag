3f4d35084da3df3e9e8194d26db9593283fd9298
magalig0 <162461866+magalig0@users.noreply.github.com>
2024-09-02 12:47:19 -0300

    Add files via upload

---
+++ pag/script.js
@@ -0,0 +1,11 @@
+document.getElementById("campo_1").addEventListener("click", function() {
+    window.location.href = "pagina1.html";
+});
+
+document.getElementById("campo_2").addEventListener("click", function() {
+    window.location.href = "pagina2.html";
+});
+
+document.getElementById("campo_3").addEventListener("click", function() {
+    window.location.href = "pagina3.html";
+});
